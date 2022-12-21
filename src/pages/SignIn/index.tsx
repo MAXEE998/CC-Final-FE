c08e09e0355e6ff70b1062a40c8df5378255a885
Hongyi Li <hongyi.li@sezzle.com>
2022-12-21 02:36:38 -0500

    fix: redirect issue

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -34,7 +34,7 @@ export default function SignIn(props: Props) {
         username: "Hongyi"
       };
       ctx.openSnackBar?.(`Success, welcome back ${user.username}!`, "success");
-      ctx.navigate?.(redirectTo || `/patient/dashboard/`);
+      ctx.navigate?.(redirectTo || `/${isPatient? "patient": "doctor"}/dashboard/`);
     } catch(err) {
       ctx.openSnackBar?.(`Error: ${err}`, "error");
     }
